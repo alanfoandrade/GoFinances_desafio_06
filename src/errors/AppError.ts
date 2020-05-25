2c51bce2151cf58b30b55798b3c56e3bdd95c4a9
Alan Andrade <alaninatel@gmail.com>
2020-05-25 20:31:24 -0300

    Desafio Finalizado

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,11 +1,11 @@
 class AppError {
   public readonly message: string;
 
-  public readonly statusCode: number;
+  public readonly status: number;
 
   constructor(message: string, statusCode = 400) {
     this.message = message;
-    this.statusCode = statusCode;
+    this.status = statusCode;
   }
 }
 
